13df8eeaea63da58e6486b2d0b53e94613b4380e
[Zack Hamza] <[zack.hamza2001@gmail.com]>
2021-08-20 20:01:33 -0700

    Added alerts for empty fields and booklists.

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        // Make alert vanish after 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -71,17 +82,28 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instatiating Book
-    const book = new Book(title, author, isbn);
+    //Validating input fields
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert("Please fill in all fields.", "danger");
+    } else {
+        // Instantiating Book
+        const book = new Book(title, author, isbn);
 
-    //Add book to UI
-    UI.addBookToList(book);
+        //Add book to UI
+        UI.addBookToList(book);
 
-    // Clear search fields
-    UI.clearFields();
+        //Success alert for adding
+        UI.showAlert("Book added successfully.", "success");
+
+        // Clear input fields
+        UI.clearFields();
+    }
 });
 
 // Event: Remove a book
 document.querySelector("#book-list").addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+
+    //Success alert for deleting
+    UI.showAlert("Book removed successfully.", "success");
 })
